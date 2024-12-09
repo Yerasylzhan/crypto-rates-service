f9560fde57dcdaeaf1dfb503a3600907f06c5f1b
Yerasylzhan <asikasik9000@gmail.com>
2024-12-10 03:28:39 +0600

    CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  app.enableCors({
+    origin: 'http://localhost:3003',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Crypto Rates Service')
     .setDescription('API для получения и управления крипто-курсами')
